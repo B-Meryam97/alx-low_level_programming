4abc19cc6808ab4a80234ee08c0b634d9f6ce921
B-Meryam97 <miyamies1997@gmail.com>
2023-07-13 08:01:27 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - main function
+ * main - This is the main function
  *
- * return: 0
+ * return: always 0
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle"); return (0);
