bf9b9194a8155a0845e384909cf28f4e00115936
B-Meryam97 <miyamies1997@gmail.com>
2023-07-14 16:35:43 -0700

    double alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-        int n = 97;
+	int n = 97;
 	int o = 65;
-
-        while (n <= 122)
-        {
-                putchar(n);
-                n++;
-        }
+	
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
 	while (o <= 90)
-        {
-                putchar(o);
-                o++;
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(o);
+		o++
+	}
+	putchar('\n');
+	return (0);
 }
