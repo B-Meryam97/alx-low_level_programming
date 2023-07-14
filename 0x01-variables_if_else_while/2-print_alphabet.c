399ad92829ca61c8b9c0f6ad5f757f8bd1cd134a
B-Meryam97 <miyamies1997@gmail.com>
2023-07-14 16:00:02 -0700

    I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	for (m = "a" ; m <= "z" ; ++m)
+		putchar(m);
+	putchar("\n");
+	return (0);
+}
