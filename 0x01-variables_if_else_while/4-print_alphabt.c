2f8f359d26a0d6c93e079f98621fbb48a7ee86d9
B-Meryam97 <miyamies1997@gmail.com>
2023-07-14 16:52:00 -0700

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,12 +5,17 @@
  *
  * Return: 0 (success)
  */
+
 int main(void)
 {
 	int n = 97;
-
 	while (n <= 122)
 	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
 		putchar(n);
 		n++;
 	}
