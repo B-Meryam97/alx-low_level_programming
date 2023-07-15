a82cdbeeb18b9622c3d93b2fcfd179ae12ffb655
B-Meryam97 <miyamies1997@gmail.com>
2023-07-14 18:27:20 -0700

    less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = i + 1 ; j <= 9; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
-			
+
 			if (i != 8 || j != 9)
 			{
 				putchar(',');
@@ -26,5 +26,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
