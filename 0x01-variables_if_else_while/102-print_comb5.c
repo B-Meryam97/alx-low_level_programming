52ef23d5d30fe36244291c0bd8d08c44263d68e9
B-Meryam97 <miyamies1997@gmail.com>
2023-07-14 18:55:37 -0700

    last one for the day

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,16 +15,19 @@ int main(void)
 	{
 		for (y = x ; y <= 99 ; y++)
 		{
-			putchar((x / 10) + '0');
-			putchar((x % 10) + '0');
-			putchar(' ');
-			putchar((y / 10) + '0');
-			putchar((y % 10) + '0');
-
-			if (x != 98 || y != 99)
+			if (x != y)
 			{
-				putchar(',');
+				putchar((x / 10) + '0');
+				putchar((x % 10) + '0');
 				putchar(' ');
+				putchar((y / 10) + '0');
+				putchar((y % 10) + '0');
+				
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
