667489aac19d04decc30332fa33e04812bd00481
B-Meryam97 <miyamies1997@gmail.com>
2023-07-14 18:37:49 -0700

    do more of what you do

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int m;
+	int n;
+	int o;
+
+	for (m = 0 ; m <= 7 ; m++)
+	{
+		for (n = m + 1 ; n <= 8 ; n++)
+		{
+			for (o = n + 1 ; o <= 9 ; o++)
+			{
+				putchar('0' + m);
+				putchar('0' + n);
+				putchar('0' + o);
+
+				if (m!= 7 || n != 8 || o != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
