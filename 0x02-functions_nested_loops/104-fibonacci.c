c95e1558f6890258d770fb9fe36e8d476234b978
B-Meryam97 <miyamies1997@gmail.com>
2023-07-18 09:54:48 -0700

    class

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,26 +6,26 @@
  */
 void print_fibonacci(int n)
 {
-	unsigned long fib1 = 1;
-	unsigned long fib2 = 2;
-	unsigned long fib;
-	int i;
+    unsigned long fib1 = 1;
+    unsigned long fib2 = 2;
+    unsigned long fib;
+    int i;
 
-	if (n >= 1)
-		printf("%lu", fib1);
+    if (n >= 1)
+        printf("%lu", fib1);
 
-	if (n >= 2)
-		printf(", %lu", fib2);
+    if (n >= 2)
+        printf(", %lu", fib2);
 
-	for (i = 3; i <= n; i++)
-	{
-		fib = fib1 + fib2;
-		printf(", %lu", fib);
-		fib1 = fib2;
-		fib2 = fib;
-	}
+    for (i = 3; i <= n; i++)
+    {
+        fib = fib1 + fib2;
+        printf(", %lu", fib);
+        fib1 = fib2;
+        fib2 = fib;
+    }
 
-	printf("\n");
+    printf("\n");
 }
 
 /**
@@ -35,6 +35,6 @@ void print_fibonacci(int n)
  */
 int main(void)
 {
-	print_fibonacci(98);
-	return (0);
+    print_fibonacci(98);
+    return (0);
 }
