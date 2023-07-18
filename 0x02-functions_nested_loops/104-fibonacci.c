0d320f3fa816273f9dca728736aa64f6fe0ee530
B-Meryam97 <miyamies1997@gmail.com>
2023-07-18 09:34:50 -0700

    computer class

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,30 @@
-#include "main.h"
-#include <stdio.h>
-#include <unistd.h>
+#include <unistd.h>  // For write function
+#include <stdio.h>   // For printf and putchar functions
 
 /**
  * print_fibonacci - Prints the first n Fibonacci numbers
- * @n: number of fib numbers to print
+ * @n: The number of Fibonacci numbers to print
  */
-
 void print_fibonacci(int n)
 {
 	unsigned long fib1 = 1;
 	unsigned long fib2 = 2;
 
+	// Print the first two Fibonacci numbers separately
 	printf("%lu, %lu, ", fib1, fib2);
 
-	for (int i = 3; i <= n; i++)
+	// Print the remaining Fibonacci numbers
+	int i;
+	for (i = 3; i <= n; i++)
 	{
 		unsigned long fib = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = fib;
 
+		// Print the current Fibonacci number
 		printf("%lu", fib);
 
+		// Add comma and space after the number if it's not the last one
 		if (i != n)
 		{
 			putchar(',');
@@ -33,13 +36,13 @@ void print_fibonacci(int n)
 }
 
 /**
- * main -Entry point
+ * main - Entry point of the program
  *
- * Return: 0 (Success)
+ * Return: Always 0
  */
-
 int main(void)
 {
 	print_fibonacci(98);
-	return (0);
+	return 0;
 }
+
