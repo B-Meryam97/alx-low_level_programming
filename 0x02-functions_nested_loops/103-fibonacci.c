eb66942064db0f0051ba3d0446faea3999535cd5
B-Meryam97 <miyamies1997@gmail.com>
2023-07-18 09:02:40 -0700

    ever liber abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 0;
+
+	while (fib1 <= 4000000)
+	{
+		if (fib1 % 2 == 0)
+			even_sum += fib1;
+	
+	fib_sum = fib1 + fib2;
+	fib1 = fib2;
+	fib2 = fib_sum;
+	}
+
+	printf("%lu\n", even_sum);
+
+	return (0);
+}
