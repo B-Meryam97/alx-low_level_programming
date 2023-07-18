0257398106942d2fb6aaa42092eadcc7a215133c
B-Meryam97 <miyamies1997@gmail.com>
2023-07-18 08:45:37 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int n;
 	unsigned int fib[50];
-	
+
 	fib[0] = 1;
 	fib[1] = 2;
 
 	printf("%u, %u", fib[0], fib[1]);
 
-	for (n = 2; n < 49; n++)
+	for (n = 2 ; n < (50 ; n++)
 	{
 		fib[n] = fib[n - 1] + fib[n - 2];
 		printf(", %u", fib[n]);
