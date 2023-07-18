e0ba305756b9ef530ae8720a1b5c612dd445579b
B-Meryam97 <miyamies1997@gmail.com>
2023-07-18 08:57:26 -0700

    fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,18 +11,18 @@ int main(void)
 {
 	int i;
 	unsigned long fib[50];
-	
+
 	fib[0] = 1;
 	fib[1] = 2;
-	
+
 	printf("%lu, %lu", fib[0], fib[1]);
 	for (i = 2; i < 50; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 		printf(", %lu", fib[i]);
 	}
-	
+
 	printf("\n");
-	
+
 	return (0);
 }
