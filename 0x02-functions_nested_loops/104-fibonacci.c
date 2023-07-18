56b23f75ff34d2ceb74ca056466a3adbbba8ee5e
B-Meryam97 <miyamies1997@gmail.com>
2023-07-18 09:18:39 -0700

    computer class

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,37 @@
 #include "main.h"
 #include <stdio.h>
+#include <gmp.h>
 
 /**
  * main -Entry point
  *
- * Rturn: 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2, fib_sum;
+	mpz_t fib1, fib2, fib_sum;
 	int count;
 
-	printf("%lu, %lu", fib1, fib2);
+	mpz_init_set_ui(fib1, 1);
+	mpz_init_set_ui(fib2, 2);
+	mpz_init(fib_sum);
+
+	gmp_printf("%Zd, %Zd", fib1, fib2);
 
 	for (count = 2; count < 98; count++)
 	{
-		fib_sum = fib1 + fib2;
-		printf(", %lu", fib_sum);
-		fib1 = fib2;
-		fib2 = fib_sum;
+		mpz_add(fib_sum, fib1, fib2);
+
+		gmp_printf(", %Zd", fib_sum);
+
+		mpz_set(fib1, fib2);
+		mpz_set(fib2, fib_sum);
 	}
 
 	printf("\n");
+
+	mpz_clears(fib1, fib2, fib_sum, NULL);
 
 	return (0);
 }
