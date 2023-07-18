bba63ea24b015aa401a2e56df721c010429209c1
B-Meryam97 <miyamies1997@gmail.com>
2023-07-18 08:01:24 -0700

    timetables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,17 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (column = 0; column <= 9; column++)
+		_putchar('0'); /* Print the first column */
+		for (column = 1; column <= 9; column++)
 		{
 			product = row * column;
-			if (column == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+			if (product < 10)
 				_putchar(' ');
-				if (product < 10)
-					_putchar(' ');
+			else
 				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
