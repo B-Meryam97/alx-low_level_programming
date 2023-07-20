9364d0dea77560a3e6d990ead46ec0e6a74b8018
B-Meryam97 <miyamies1997@gmail.com>
2023-07-20 06:50:03 -0700

    multiplicatipn

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integrers
+ * @a: first integrer to be multiplied
+ * @b: second integrer to be multiplied
+ *
+ * Return: the product of the two integrers
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
