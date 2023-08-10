449c47412740ef9aad85945aac6757423f328531
B-Meryam97 <miyamies1997@gmail.com>
2023-08-10 06:13:13 -0700

    trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,13 +11,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+        void *ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
+        {
 		fprintf(stderr, "Memory allocation failed\n");
 		exit(98);
-	}
+        }
 
 	return (ptr);
 }
