f29b23757c3a0ba6570226be6770a1bc0e762726
B-Meryam97 <miyamies1997@gmail.com>
2023-08-14 05:29:41 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,18 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a struct called dog.
+ * @name: first member.
+ * @age: second member.
+ * @owner: third member.
+ */
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+	char *name; /**< The name of the dog */
+	float age; /**< The age of the dog */
+	char *owner; /**< The owner of the dog */
 };
+
+#endif /* DOG_H */
